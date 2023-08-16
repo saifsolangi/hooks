a50ad86d19e1e887e7e1742219974c4f1257418d
Saif Ali <saifsolangi2@gmail.com>
2023-08-16 11:27:23 +0500

    added new example to understand use State

--- src/components/StateHook.js
+++ src/components/StateHook.js
@@ -9,9 +9,29 @@ export default function StateHook() {
         setCounter(counter + 1)
         console.log(counter)
     }
+
+    const showText = (event) => {
+        setInputVal(event.target.value)
+    }
+    // let inputVal = "Saify"
+
+    const [inputVal, setInputVal] = useState("saify")
     return (
-        <div>
-            {counter} <button onClick={incrementNumber}>Increment</button>
-        </div>
+        <>
+
+            {/* example 1 */}
+            <div>
+                {counter} <button onClick={incrementNumber}>Increment</button>
+            </div>
+
+
+            {/* example 2 */}
+            <br />
+            <div>
+                <input type='text' placeholder='type something...' onChange={showText} /><br />
+                {inputVal}
+            </div>
+
+        </>
     )
 }
